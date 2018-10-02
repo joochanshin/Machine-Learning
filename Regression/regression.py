981c0b0ac27147d7301b080831ce4fe3becaa3d4
JooChan <14jshin@gmail.com>
2018-10-02 05:01:02 -0400

    added a forecast function

--- Regression/regression.py
+++ Regression/regression.py
@@ -2,6 +2,7 @@
 import config
 import pandas as pd
 import quandl
+import math
 
 quandl.ApiConfig.api_key = config.api_key
 df = quandl.get('WIKI/GOOGL', rows=5)
@@ -12,5 +13,13 @@ df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.
 #Percent change is (new-old)/old
 
 df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
+
+forecast_col = "Adj. Close"
+df.fillna(-99999, inplace=True) #na=not available. You cant use NaN in ML or sacrifice data
+
+forecast_out = int(math.ceil(0.01*len(df)))
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+df.dropna(inplace=True)
 
 print(df.head)
