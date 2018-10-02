992a84ac9681c967f4e41a854fc495dfcbcceaca
JooChan <14jshin@gmail.com>
2018-10-01 21:50:18 -0400

    played with data frames

--- Regression/regression.py
+++ Regression/regression.py
@@ -1,8 +1,16 @@
 #regression stock prices
-
+import config
 import pandas as pd
 import quandl
 
-df = quandl.get('WIKI/GOOGL')
+quandl.ApiConfig.api_key = config.api_key
+df = quandl.get('WIKI/GOOGL', rows=5)
+df = df[['Adj. Open', 'Adj. High', 'Adj. Low', 'Adj. Close', 'Adj. Volume']]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Low']) / df['Adj. Low'] * 100.0
+#High - low percent, the percent volatiltiy. 
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+#Percent change is (new-old)/old
+
+df = df[['Adj. Close','HL_PCT','PCT_change','Adj. Volume']]
 
 print(df.head)
